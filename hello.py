394a93add8050b23b62287857e40fad5cc34deda
Benjamin <benjamin.cornejo.meneque@gmail.com>
2022-10-20 21:41:43 +0800

    add some new functions

--- hello.py
+++ hello.py
@@ -1,9 +1,17 @@
 def main():
     greetings()
+    bye()
+
 
 def greetings():
     name = input("Name: ")
-    print(f'hello, {name}')
+    print(f"hello, {name}")
+
+
+def bye():
+    name = input("Name: ")
+    print(f"goodbye, {name}")
+
 
 if __name__ == "__main__":
     main()
